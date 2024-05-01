81577696efaf14bd0142936e4a0e30f70ad678b5
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2024-05-01 19:01:46 +0100

    refactor: Outputs an error message when using the program with arguments different than 2.

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,15 +6,17 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 23:53:46 by arabelo-          #+#    #+#             */
-/*   Updated: 2024/04/19 10:06:08 by arabelo-         ###   ########.fr       */
+/*   Updated: 2024/05/01 18:59:03 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Harl.hpp>
 
 int	main(int ac, char **av) {
-	if (ac != 2)
+	if (ac != 2) {
+		std::cerr << "Error: program usage -> ./Harl 'ARGS'" << std::endl;
 		return (1);
+	}
 	Harl	harl;
 	std::string	level(*(av + 1));
 
