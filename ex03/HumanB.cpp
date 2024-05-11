409497a2072cce11ec023735d286de8473317541
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2024-05-11 19:05:46 +0100

    fix: Changes the HumanB contructor to initalizes the pointer to the Weapon class to NULL and the message of the attack member function to check if the pointer is set to NULL.

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -3,24 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   HumanB.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: arabelo- <arabelo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 13:18:32 by arabelo-          #+#    #+#             */
-/*   Updated: 2024/04/08 14:05:51 by arabelo-         ###   ########.fr       */
+/*   Updated: 2024/05/11 18:17:21 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <HumanB.hpp>
 
 HumanB::HumanB(std::string name): _name(name) {
-	
+	_weapon = NULL;
 }
 
 HumanB::~HumanB() {
 }
 
 void	HumanB::attack(void) {
-	std::cout << _name << " " << "attacks with their " << _weapon->getType() << std::endl;
+	if (_weapon)
+		std::cout << _name << " " << "attacks with their " << _weapon->getType() << std::endl;
+	else
+		std::cout << _name << " " << "attacks with no weapon" << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon &type) {
